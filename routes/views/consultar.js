05d20184080570f81d83625579d1bf80e5443cc9
Guido Canevello <guidocanevello@hotmail.com>
2020-10-24 14:11:07 -0300

    Avances en Desarrollo

---
+++ routes/views/consultar.js
@@ -0,0 +1,32 @@
+const keystone = require("keystone")
+
+exports = module.exports = function (req, res) {
+    let view = new keystone.View(req, res);
+    let locals = res.locals;
+
+    // Set locals
+    locals.sections = 'consultar';
+
+    // Carga de Datos
+    locals.data = {
+        // alumnos: []
+        booleanTest: true,
+    }
+
+    view.on('init', function (next) {
+        let q = keystone.list('Alumno').model.findOne({
+            dni: "39.682.936"
+        });
+        q.exec(function (err, result) {
+            locals.data.alumno = result;
+            next(err);
+        })
+    })
+
+    // // Load Products
+    // view.query('alumnos', keystone.list('Alumno').model.find());
+
+    // Render View
+    view.render('consultar');
+}
+
